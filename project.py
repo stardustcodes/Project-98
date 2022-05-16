3f7c2de8e12b21d6d10d138dca94dba347ff89dd
iris <71960674+stardustcodes@users.noreply.github.com>
2022-05-16 19:01:33 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,9 @@
+a=open('C:/Users/Dell/OneDrive/Desktop/my files/work/coding stuff/Python/functions/test.txt')
+b=a.read()
+c=open('C:/Users/Dell/OneDrive/Desktop/my files/work/coding stuff/Python/functions/text.txt')
+d=c.read()
+
+e=open('C:/Users/Dell/OneDrive/Desktop/my files/work/coding stuff/Python/functions/test.txt', 'w')
+e.write(d) 
+f=open('C:/Users/Dell/OneDrive/Desktop/my files/work/coding stuff/Python/functions/text.txt', 'w')
+f.write(b) 
